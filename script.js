2c7f5b77641b4811343941e4cad7de4b59d2fd84
Tyler Bartle <tman.bartleboy1@gmail.com>
2024-05-08 00:03:15 -0600

    Commented JS

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
-let typeSelector = document.getElementById("unittype");
-let topInput = document.getElementById("topinput");
-let bottomInput = document.getElementById("bottominput");
-let topUnitSelector = document.getElementById("topunitselector");
-let bottomUnitSelector = document.getElementById("bottomunitselector");
-const classes = ["Area", "Mass", "Length", "Volume"]
-function hideUnwantedUnits() {
-    let toUnHide = document.getElementsByClassName(typeSelector.value);
+let typeSelector = document.getElementById("unittype");                 // Unit type dropdown
+let topInput = document.getElementById("topinput");                     // top input box
+let bottomInput = document.getElementById("bottominput");               // bottom input box
+let topUnitSelector = document.getElementById("topunitselector");       // top unit dropdown
+let bottomUnitSelector = document.getElementById("bottomunitselector"); // bottom unit dropdowm
+const classes = ["Area", "Mass", "Length", "Volume"];                   // list of all possible unit types, used as HTML classes
+function hideUnwantedUnits() { // remove unrelated units from possible selection
+    let toUnHide = document.getElementsByClassName(typeSelector.value); // all units to show after being hidden previously
     for (let unit of toUnHide) {
-        unit.classList.remove("hidden")
+        // remove the "hidden" class, allowing the unit to show again
+        unit.classList.remove("hidden");
     };
 
-    let notThisClass = classes.filter(className => className !== typeSelector.value);
+    let notThisClass = classes.filter(className => className !== typeSelector.value); // new list, containing all unit types apart from the selected one
     for (let className of notThisClass) {
-        let toHide = document.getElementsByClassName(className);
+        let toHide = document.getElementsByClassName(className); // all units of one of the three above types
         for (let unit of toHide) {
+            // add the "hidden" class, removing the unit from the list
             unit.classList.add("hidden");
-        }
+        };
     };
 
-    let unitSelectors = document.getElementsByClassName("unitselector")
+    let unitSelectors = document.getElementsByClassName("unitselector"); // both unit selection dropdowns
     for (let unitSelector of unitSelectors) {
+        // set the value of the dropdown to nothing
         unitSelector.value = "";
-    }
+    };
 
-    let numberInputs = document.getElementsByClassName("numberinput")
+    let numberInputs = document.getElementsByClassName("numberinput"); // both number inputs
     for (let numberInput of numberInputs) {
+        // set the value of the input to nothing
         numberInput.value = "";
-    }
+    };
 };
+// run hideUnwantedUnits every time typeSelector is changed
 typeSelector.addEventListener("change", (e) => hideUnwantedUnits());
 
+// dictionary containing the unit conversion ratios
 const unitDict = new Map([
     ["Square kilometer", 1],
     ["Square meter", 1000000],
@@ -84,18 +90,24 @@ const unitDict = new Map([
     ["Cubic inch", 231],
 ]);
 
+// convert the content of the top box to the lower unit
 function convertTopToBottom() {
     let converted = parseFloat(topInput.value) / unitDict.get(topUnitSelector.value) * unitDict.get(bottomUnitSelector.value);
     bottomInput.value = isNaN(converted) ? "" : converted;
 }
+// convert the content of the bottom box to the above unit
 function convertBottomToTop() {
     let converted = parseFloat(bottomInput.value) / unitDict.get(bottomUnitSelector.value) * unitDict.get(topUnitSelector.value);
     topInput.value = isNaN(converted) ? "" : converted;
 }
+
+// convert units everry time the content of either input box is changed
 topInput.addEventListener("input", (e) => convertTopToBottom());
 bottomInput.addEventListener("input", (e) => convertBottomToTop());
 
+// convert units every time either unit dropdown is changed
 topUnitSelector.addEventListener("change", (e) => convertBottomToTop());
 bottomUnitSelector.addEventListener("change", (e) => convertTopToBottom());
 
+// hide unwanted units on page load
 hideUnwantedUnits();
